cf31ea3b07343d4ad616b333a23bc169281cd644
bandthedoge <bandithedoge@protonmail.com>
2022-08-16 22:25:34 +0200

    add luaPackages.lua-dbus

--- pkgs/luaPackages/default.nix
+++ pkgs/luaPackages/default.nix
@@ -1,10 +1,18 @@
-{
-  pkgs,
-  sources,
+{ pkgs
+, sources
+,
 }: rec {
   lua-dbus_proxy = pkgs.luaPackages.buildLuarocksPackage rec {
     inherit (sources.lua-dbus_proxy) src pname version;
-    propagatedBuildInputs = with pkgs.luaPackages; [lgi];
+
+    propagatedBuildInputs = with pkgs.luaPackages; [ lgi ];
     knownRockspec = src + "/rockspec/dbus_proxy-devel-1.rockspec";
+  };
+
+  lua-dbus = pkgs.luaPackages.buildLuarocksPackage rec {
+    inherit (sources.lua-dbus) src pname version;
+
+    propagatedBuildInputs = with pkgs.luaPackages; [ ldbus ];
+    knownRockspec = src + "/${pname}-scm-0.rockspec";
   };
 }
