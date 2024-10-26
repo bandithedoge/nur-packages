5ab4a08ecebc248e93a70452f9022227aef7d9be
bandithedoge <bandithedoge@protonmail.com>
2024-10-27 01:05:00 +0200

    emacsPackages.indent-bars: add compat dependency

--- pkgs/emacsPackages/_overrides.nix
+++ pkgs/emacsPackages/_overrides.nix
@@ -2,4 +2,8 @@
   eglot-ltex = prev.eglot-ltex.overrideAttrs (_: {
     buildInputs = with pkgs.emacsPackages; [f];
   });
+
+  indent-bars = prev.indent-bars.overrideAttrs (_: {
+    buildInputs = with pkgs.emacsPackages; [compat];
+  });
 }
