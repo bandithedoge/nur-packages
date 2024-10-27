d09061c14abd77044fba7b6e774570e06f09fe0a
bandithedoge <bandithedoge@protonmail.com>
2024-10-27 19:32:09 +0100

    tal.bassline-101: add fontconfig dependency

--- pkgs/tal/default.nix
+++ pkgs/tal/default.nix
@@ -46,6 +46,7 @@ in {
     source = sources.bassline-101;
     description = "TAL-BassLine-101 is an accurate emulation of the popular SH 101";
     homepage = "https://tal-software.com/products/tal-bassline-101";
+    extraLibs = with pkgs; [fontconfig];
   };
 
   chorus-lx = mkTal {
