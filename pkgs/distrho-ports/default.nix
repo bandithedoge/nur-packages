26502bbf750f12cdb01eea056340e38045c3dccc
bandithedoge <bandithedoge@protonmail.com>
2025-01-17 19:02:45 +0100

    distrho-ports: allow specifying plugins to build

--- pkgs/distrho-ports/default.nix
+++ pkgs/distrho-ports/default.nix
@@ -1,40 +1,47 @@
 {
   pkgs,
   sources,
+  plugins ? [], # empty means build all
   ...
-}:
-pkgs.stdenv.mkDerivation {
-  inherit (sources.distrho-ports) pname src;
-  version = sources.distrho-ports.date;
+}: let
+  stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+in
+  stdenv.mkDerivation {
+    inherit (sources.distrho-ports) pname src;
+    version = sources.distrho-ports.date;
 
-  nativeBuildInputs = with pkgs; [
-    meson
-    ninja
-    pkg-config
-  ];
+    nativeBuildInputs = with pkgs; [
+      meson
+      ninja
+      pkg-config
+    ];
 
-  buildInputs = with pkgs; [
-    alsa-lib
-    fftwFloat
-    freetype
-    libGL
-    xorg.libX11
-    xorg.libXext
-    xorg.libXcursor
-    xorg.libXrender
-    xorg.libXinerama
-  ];
+    buildInputs = with pkgs; [
+      alsa-lib
+      fftwFloat
+      freetype
+      libGL
+      xorg.libX11
+      xorg.libXext
+      xorg.libXcursor
+      xorg.libXrender
+      xorg.libXinerama
+    ];
 
-  prePatch = ''
-    sed -i '1s;^;#include <utility>\n;' libs/juce6.1/source/modules/juce_gui_basics/windows/juce_ComponentPeer.h
-    patchShebangs scripts/generate-lv2.sh
-  '';
+    prePatch = ''
+      sed -i '1s;^;#include <utility>\n;' libs/juce6.1/source/modules/juce_gui_basics/windows/juce_ComponentPeer.h
+      patchShebangs scripts/generate-lv2.sh
+    '';
 
-  NIX_CFLAGS_COMPILE = ["-fpermissive"];
+    mesonFlags =
+      ["-Dlto-optimizations=true"]
+      ++ pkgs.lib.optional (plugins != []) "-Dplugins=[${pkgs.lib.concatMapStringsSep "," (x: "\"${x}\"") plugins}]";
 
-  meta = with pkgs.lib; {
-    description = "Linux audio plugins and LV2 ports";
-    homepage = "https://github.com/DISTRHO/DISTRHO-Ports";
-    platforms = platforms.linux;
-  };
-}
+    NIX_CFLAGS_COMPILE = ["-fpermissive"];
+
+    meta = with pkgs.lib; {
+      description = "Linux audio plugins and LV2 ports";
+      homepage = "https://github.com/DISTRHO/DISTRHO-Ports";
+      platforms = platforms.linux;
+    };
+  }
