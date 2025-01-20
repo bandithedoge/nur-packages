5115063ee176ab4a5439cda005d77360b70b08ed
bandithedoge <bandithedoge@protonmail.com>
2025-01-20 23:55:22 +0100

    snyk-ls-bin: add meta

--- pkgs/snyk-ls-bin/default.nix
+++ pkgs/snyk-ls-bin/default.nix
@@ -16,4 +16,18 @@ in
       cp $src $out/bin/snyk-ls
       chmod +x $out/bin/snyk-ls
     '';
+
+    meta = with pkgs.lib; {
+      description = "Language Server used by IDEs as Snyk Backend for Frontends";
+      homepage = "https://github.com/snyk/snyk-ls";
+      license = licenses.asl20;
+      platforms = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      sourceProvenance = [sourceTypes.binaryNativeCode];
+    };
   }
