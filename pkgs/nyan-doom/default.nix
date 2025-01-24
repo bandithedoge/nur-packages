d4c62f0d9dc3e95cb8ea119c008d072f436d96e2
bandithedoge <bandithedoge@protonmail.com>
2025-01-24 17:03:20 +0100

    add nyan-doom

---
+++ pkgs/nyan-doom/default.nix
@@ -0,0 +1,34 @@
+{
+  pkgs,
+  sources,
+  ...
+}:
+pkgs.stdenv.mkDerivation rec {
+  inherit (sources.nyan-doom) pname version src;
+  sourceRoot = "${src.name}/prboom2";
+
+  nativeBuildInputs = with pkgs; [
+    cmake
+  ];
+
+  buildInputs = with pkgs; [
+    SDL2
+    SDL2_image
+    SDL2_mixer
+    dumb
+    fluidsynth
+    libGLU
+    libmad
+    libvorbis
+    libzip
+    portmidi
+    zlib
+  ];
+
+  meta = with pkgs.lib; {
+    description = "The most cuddly Doom Source Port, with an emphasis on innovative and quality-of-life features";
+    homepage = "https://github.com/andrikpowell/nyan-doom";
+    license = licenses.gpl2Plus;
+    platforms = platforms.unix;
+  };
+}
