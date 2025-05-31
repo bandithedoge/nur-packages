e2a09a796068638f1419e16cb3dbd68fbe4dae5d
bandithedoge <bandithedoge@protonmail.com>
2025-05-31 16:41:59 +0200

    beetsPackages: don't propagate beets

--- pkgs/beetsPackages/default.nix
+++ pkgs/beetsPackages/default.nix
@@ -7,8 +7,11 @@
   describe = pkgs.python3Packages.buildPythonApplication {
     inherit (sources.beets-describe) pname version src;
 
+    nativeBuildInputs = with pkgs; [
+      beets
+    ];
+
     dependencies = with pkgs.python3Packages; [
-      pkgs.beets
       numpy
       pandas
       terminaltables
@@ -22,13 +25,23 @@
       six
       pyyaml
     ];
+
+    meta = with pkgs; {
+      description = "A beets plugin for obsessive-compulsive music geeks to describe what's in their library";
+      homepage = "https://github.com/adamjakab/BeetsPluginDescribe";
+      license = lib.licenses.mit;
+      inherit (beets.meta) platforms;
+    };
   };
 
   yearfixer = pkgs.python3Packages.buildPythonApplication {
     inherit (sources.beets-yearfixer) pname version src;
 
+    nativeBuildInputs = with pkgs; [
+      beets
+    ];
+
     dependencies = with pkgs.python3Packages; [
-      pkgs.beets
       requests
     ];
 
@@ -39,5 +52,12 @@
       six
       pyyaml
     ];
+
+    meta = with pkgs; {
+      description = "A beets plugin for obsessive-compulsive music geeks to fix missing album release date";
+      homepage = "https://github.com/adamjakab/BeetsPluginYearFixer";
+      license = lib.licenses.mit;
+      inherit (beets.meta) platforms;
+    };
   };
 }
