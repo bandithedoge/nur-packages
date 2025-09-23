3049f94c279f77adc05e18501fed02d01a83b8f1
bandithedoge <bandithedoge@protonmail.com>
2025-09-23 20:43:00 +0200

    add nodePackages.some-sass-language-server

--- pkgs/nodePackages/default.nix
+++ pkgs/nodePackages/default.nix
@@ -3,8 +3,11 @@ let
   nodeDeps = pkgs.callPackage ./_node2nix/default.nix { nodejs = pkgs.nodejs_latest; };
 in
 {
-  inherit (nodeDeps) emmet-ls;
+  inherit (nodeDeps)
+    emmet-ls
+    some-sass-language-server
+    ;
 
-  tailwindcss-language-server = nodeDeps."@tailwindcss/language-server";
   emmet-language-server = nodeDeps."@olrtg/emmet-language-server";
+  tailwindcss-language-server = nodeDeps."@tailwindcss/language-server";
 }
