2cf4b0e8ff76c912f0fb31f8c88c3b3f2c8822f4
bandithedoge <bandithedoge@protonmail.com>
2025-09-25 19:19:31 +0200

    dsp56300: override installPhase

--- pkgs/dsp56300/default.nix
+++ pkgs/dsp56300/default.nix
@@ -3,14 +3,15 @@
   sources,
   utils,
   variants ? [
+    "nodalred2x"
     "osirus"
     "ostirus"
     "vavra"
     "xenia"
-    "nodalred2x"
   ],
   ...
 }:
+assert builtins.length variants != 0;
 utils.juce.mkJucePackage {
   inherit (sources.dsp56300) pname version src;
 
@@ -25,11 +26,33 @@ utils.juce.mkJucePackage {
     substituteAll CMakeLists.txt --replace-fail "/usr/local" "${placeholder "out"}"
   '';
 
+  installPhase = ''
+    mkdir -p $out/{bin,lib}
+  ''
+  + pkgs.lib.optionalString (builtins.elem "osirus" variants || builtins.elem "ostirus" variants) ''
+    cp source/virusTestConsole/virusTestConsole $out/bin
+  ''
+  + pkgs.lib.optionalString (builtins.elem "vavra" variants) ''
+    cp source/mqTestConsole/mqTestConsole $out/bin
+  ''
+  + pkgs.lib.optionalString (builtins.elem "xenia" variants) ''
+    cp source/xtTestConsole/xtTestConsole $out/bin
+  ''
+  + pkgs.lib.optionalString (builtins.elem "nodalred2x" variants) ''
+    cp source/nord/n2x/n2xTestConsole/n2xTestConsole $out/bin
+  ''
+  + ''
+    cd ../bin/plugins/Release
+    cp -r CLAP $out/lib/clap
+    cp -r VST3 $out/lib/vst3
+    cp -r VST $out/lib/vst
+  '';
+
   cmakeFlags =
     let
       enable = name: cond: "gearmulator_${pkgs.lib.toUpper name}=${if cond then "on" else "off"}";
     in
-    (map (v: enable "SYNTH_${v}" true) variants);
+    map (v: enable "SYNTH_${v}" true) variants;
 
   dontUseJuceInstall = true;
 
