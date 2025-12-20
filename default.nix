7fa2327fedbd69694ccdb8c1ed762566beae19de
bandithedoge <bandithedoge@protonmail.com>
2025-12-20 17:22:37 +0100

    refactor removed packages

--- default.nix
+++ default.nix
@@ -4,17 +4,21 @@ let
 
   concat = pkgs.lib.concatStringsSep ".";
 in
-pkgs.lib.recursiveUpdate
-  (pkgs.lib.recursiveUpdate all (
-    pkgs.lib.mapAttrsRecursive (
-      old: new:
-      pkgs.lib.warn "${concat old} has been renamed to ${concat new}" (pkgs.lib.attrByPath new null all)
-    ) (import ./_renamed.nix)
-  ))
-  (
-    pkgs.lib.mapAttrsRecursive (
-      old: new:
-      pkgs.lib.warn "${concat old} is available in nixpkgs as ${concat new}" pkgs.lib.attrByPath new null
-        pkgs
-    ) (import ./_upstreamed.nix)
-  )
+builtins.foldl' pkgs.lib.recursiveUpdate all [
+  (pkgs.lib.mapAttrsRecursive (
+    old: new:
+    pkgs.lib.warn "${concat old} has been renamed to ${concat new}" (pkgs.lib.attrByPath new null all)
+  ) (import ./_renamed.nix))
+
+  (pkgs.lib.mapAttrsRecursive (
+    old: new:
+    pkgs.lib.warn "${concat old} is available in nixpkgs as ${concat new}" (
+      pkgs.lib.attrByPath new null pkgs
+    )
+  ) (import ./_upstreamed.nix))
+
+  (pkgs.lib.updateManyAttrsByPath (builtins.map (path: {
+    inherit path;
+    update = _: throw "${concat path} has been removed";
+  }) (import ./_removed.nix)) { })
+]
--- flake.nix
+++ flake.nix
@@ -49,7 +49,9 @@
             let
               allPackages = inputs.flake-utils.lib.flattenTree (import ./all.nix { inherit pkgs; });
 
-              buildable = pkgs.lib.filterAttrs (_: p: !(p.meta.broken || p.meta.insecure)) allPackages;
+              buildable = pkgs.lib.filterAttrs (
+                _: p: ((builtins.tryEval p).success && !(p.meta.broken || p.meta.insecure))
+              ) allPackages;
               cacheable = pkgs.lib.filterAttrs (
                 _: p:
                 (p.meta.license.free or true)
