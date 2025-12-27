b986b1f051a337581c97da1f713af922ab78f284
bandithedoge <bandithedoge@protonmail.com>
2025-12-27 17:05:45 +0100

    hvcc: add arpeggio and pydantic-extra-types deps

--- pkgs/hvcc/package.nix
+++ pkgs/hvcc/package.nix
@@ -1,5 +1,6 @@
 {
   sources,
+  fetchFromGitHub,
 
   lib,
   python3Packages,
@@ -19,6 +20,19 @@ let
       jinja2
     ];
   };
+
+  # XXX: https://github.com/NixOS/nixpkgs/pull/474587
+  pydantic-extra-types = python3Packages.pydantic-extra-types.overridePythonAttrs (old: rec {
+    version = "2.11.0";
+    src = fetchFromGitHub {
+      owner = "pydantic";
+      repo = "pydantic-extra-types";
+      tag = "v${version}";
+      hash = "sha256-aXhlfDBCpk8h3F4gXAQ40fVKxsoFvkmfO/roaqrGxho=";
+    };
+
+    dependencies = old.dependencies ++ [ python3Packages.cron-converter ];
+  });
 in
 python3Packages.buildPythonPackage {
   inherit (sources.hvcc) pname src;
@@ -27,9 +41,11 @@ python3Packages.buildPythonPackage {
   pyproject = true;
 
   dependencies = with python3Packages; [
+    arpeggio
     importlib-resources
     poetry-core
     pydantic
+    pydantic-extra-types
     wstd2daisy
   ];
 
