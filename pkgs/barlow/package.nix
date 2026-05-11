cc476facdb3c07b5ecc21a47c1daea64d30dcdfc
bandithedoge <bandithedoge@protonmail.com>
2026-05-11 20:30:04 +0200

    barlow: install fonts manually instead of using installFonts hook

--- pkgs/barlow/package.nix
+++ pkgs/barlow/package.nix
@@ -3,13 +3,17 @@
 
   lib,
   stdenv,
-  installFonts,
 }:
 stdenv.mkDerivation {
   inherit (sources.barlow) pname src;
   version = sources.barlow.date;
 
-  nativeBuildInputs = [ installFonts ];
+  buildPhase = ''
+    mkdir -p $out/share/fonts/{truetype,opentype,woff2}
+    cp -r fonts/ttf/*.ttf $out/share/fonts/truetype
+    cp -r fonts/otf/*.otf $out/share/fonts/opentype
+    cp -r fonts/ttf/*.woff2 $out/share/fonts/woff2
+  '';
 
   passthru._ignoreDupe = true;
 
