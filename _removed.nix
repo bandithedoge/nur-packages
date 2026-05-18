4dbf571fb70792cbe5af5712109dfae441fc5d58
bandithedoge <bandithedoge@protonmail.com>
2026-05-18 20:19:55 +0200

    remove fennel-language-server

--- _removed.nix
+++ _removed.nix
@@ -1,14 +1,17 @@
 [
+  # keep-sorted start block=yes
   [ "digits-bin" ]
+  [ "fennel-language-server" ]
   [ "kiwmi" ]
   [ "snyk-ls-bin" ]
   [ "tal" ]
   [ "thorium-bin" ]
   [ "venn" ]
+  [ "vkdoom" ]
+  [ "waterfox-bin" ]
   [
     "vimPlugins"
     "null-ls-nvim"
   ]
-  [ "vkdoom" ]
-  [ "waterfox-bin" ]
+  # keep-sorted end
 ]
