586bb1295a3ce388844e30fb5b860eeec912d756
bandithedoge <bandithedoge@protonmail.com>
2026-05-18 15:49:50 +0200

    add zerostack

---
+++ pkgs/zerostack/package.nix
@@ -0,0 +1,22 @@
+{
+  withAcp ? true,
+
+  sources,
+
+  lib,
+  rustPlatform,
+}:
+rustPlatform.buildRustPackage {
+  inherit (sources.zerostack) pname src;
+  version = lib.removePrefix "v" sources.zerostack.version;
+
+  cargoLock = sources.zerostack.cargoLock."Cargo.lock";
+  buildFeatures = lib.optional withAcp "acp";
+
+  meta = with lib; {
+    description = "Minimalistic coding agent written in Rust, optimized for memory footprint and performance";
+    homepage = "https://github.com/gi-dellav/zerostack";
+    license = licenses.gpl3Only;
+    platforms = platforms.unix;
+  };
+}
