365a14fff22f359cfe1636c1eed9e9bd4ebbade1
bandithedoge <bandithedoge@protonmail.com>
2026-06-22 17:03:28 +0200

    tack: use clang as linker

--- pkgs/tack/package.nix
+++ pkgs/tack/package.nix
@@ -6,19 +6,24 @@
 
   pkg-config,
   openssl,
+  wild,
+  clangStdenv,
 }:
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage.override { stdenv = clangStdenv; } {
   inherit (sources.tack) pname src;
   version = lib.removePrefix "v" sources.tack.version;
   cargoLock = sources.tack.cargoLock."Cargo.lock";
 
   nativeBuildInputs = [
     pkg-config
+    wild
   ];
 
   buildInputs = [
     openssl
   ];
+
+  env.RUSTFLAGS = "-Clinker=clang";
 
   meta = {
     description = "Flake-like toml nix pins, lazily fetched and transformed";
